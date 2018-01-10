de97071a8dbffb1cfbf5eb1e58192dd81e18775c
Thijs Van der Schaeghe <thijs@catlab.be>
2018-01-10 10:19:20 +0100

    Disable http timeout to be able to detect timeouts.

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,10 +60,11 @@ return [
             'key' => env('AWS_KEY'),
             'secret' => env('AWS_SECRET'),
             'region' => env('AWS_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => env('AWS_BUCKET')
+            /*,
             'http' => [
                 'connect_timeout' => 30
-            ]
+            ]*/
         ],
 
     ],
